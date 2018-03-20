6bf8d155529fa8c632b2dd49744bef2268845c62
Christopher Durham <cad97@cad97.com>
2018-03-20 14:18:58 -0400

    Technically functional!

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -5,24 +5,107 @@ import random
 
 class Tetris:
     def __init__(self):
-        self.grid = [[None for _ in range(10)] for _ in range(22)]
+        self.width = 10
+        self.height = 20
+        self.grid = [[None for _ in range(self.width)] for _ in range(self.height + 2)]
         self.upcoming = TetrisPiece.all
         random.shuffle(self.upcoming)
-        self.floating = self.upcoming.pop()
-        self.floating_pos = (0, 4)
+        self.floating = TetrisPiece.Null
+        self.floating_pos = (0, 0)
+        self.lock()
+        assert self._consistent()
 
-    def __str__(self):
+    def __str__(self) -> str:
         grid = list(map(lambda line: list(map(lambda block: '[]' if block else '  ', line)), self.grid))
         y, x = self.floating_pos
         for dy in range(self.floating.height):
             for dx in range(self.floating.width):
                 if self.floating.shape[dy][dx]:
-                    assert grid[y + dy][x + dx] == '  '
                     grid[y + dy][x + dx] = '{}'
         return '\n'.join(map(''.join, grid))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Tetris({self.grid})'
+
+    def left_shift(self) -> bool:
+        """Shift the floating piece one position left.
+           Returns whether the shift was successful."""
+        y, x = self.floating_pos
+        self.floating_pos = (y, x - 1)
+        if self._consistent():
+            return True
+        else:
+            self.floating_pos = (y, x)
+            return False
+
+    def right_shift(self) -> bool:
+        """Shift the floating piece one position right.
+           Returns whether the shift was successful."""
+        assert self._consistent()
+        y, x = self.floating_pos
+        self.floating_pos = (y, x + 1)
+        if self._consistent():
+            return True
+        else:
+            self.floating_pos = (y, x)
+            return False
+
+    def soft_drop(self) -> bool:
+        """Shift the floating piece one position down.
+           Returns whether the shift was successful."""
+        assert self._consistent()
+        y, x = self.floating_pos
+        self.floating_pos = (y + 1, x)
+        if self._consistent():
+            return True
+        else:
+            self.floating_pos = (y, x)
+            return False
+
+    def hard_drop(self):
+        """Shift the floating piece all the way down.
+           Does not lock the piece in place."""
+        assert self._consistent()
+        while self.soft_drop():
+            pass
+        assert self._consistent()
+
+    def lock(self) -> bool:
+        """Lock the floating piece in place and spawn the next floating piece.
+           Returns if the game is still valid and can continue."""
+        assert self._consistent()
+        y, x = self.floating_pos
+        for dy in range(self.floating.height):
+            for dx in range(self.floating.width):
+                if self.floating.shape[dy][dx]:
+                    self.grid[y + dy][x + dx] = self.floating.shape[dy][dx]
+        self.floating_pos = (0, 4)
+        self.floating = self.upcoming.pop()
+        if not self.upcoming:
+            self.upcoming = TetrisPiece.all
+            random.shuffle(self.upcoming)
+        return self._consistent()
+
+    # validity checks
+    def _consistent(self) -> bool:
+        return self._floating_inbounds() and not self._floating_overlap()
+
+    def _floating_inbounds(self) -> bool:
+        y, x = self.floating_pos
+        for dy in range(self.floating.height):
+            for dx in range(self.floating.width):
+                if self.floating.shape[dy][dx]:
+                    if not (0 <= (x + dx) < self.width and 0 <= (y + dy) < (self.height + 2)):
+                        return False
+        return True
+
+    def _floating_overlap(self) -> bool:
+        y, x = self.floating_pos
+        for dy in range(self.floating.height):
+            for dx in range(self.floating.width):
+                if self.floating.shape[dy][dx] and self.grid[y + dy][x + dx]:
+                    return True
+        return False
 
 
 class TetrisPiece:
@@ -72,6 +155,7 @@ TetrisPiece.T = TetrisPiece([[N, t, N],
 TetrisPiece.Z = TetrisPiece([[z, z, N],
                              [N, z, z],
                              [N, N, N]])
+TetrisPiece.Null = TetrisPiece([[]])
 
 del i, j, l, o, s, t, z, N
 

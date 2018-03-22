8b73e8f3c48df269ada9bd9a9873213a3bb47c70
Christopher Durham <cad97@cad97.com>
2018-03-22 10:17:53 -0400

    SRS structure

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,14 +1,22 @@
-from typing import List
+from collections import deque
+from typing import List, Tuple, Deque, Optional
 from pygame import Color
 import random
 
 
 class Tetris:
+    width: int
+    height: int
+    grid: Deque[List[Optional[Color]]]
+    upcoming: List['TetrisPiece']
+    floating: 'TetrisPiece'
+    floating_pos: Tuple[int, int]
+
     def __init__(self):
         self.width = 10
         self.height = 20
-        self.grid = [[None for _ in range(self.width)] for _ in range(self.height + 2)]
-        self.upcoming = TetrisPiece.all
+        self.grid = deque([None for _ in range(self.width)] for _ in range(self.height + 2))
+        self.upcoming = list(TetrisPiece.all)
         random.shuffle(self.upcoming)
         self.floating = TetrisPiece.Null
         self.floating_pos = (0, 0)
@@ -22,7 +30,7 @@ class Tetris:
             for dx in range(self.floating.width):
                 if self.floating.shape[dy][dx]:
                     grid[y + dy][x + dx] = '{}'
-        return '\n'.join(map(''.join, grid))
+        return '\n'.join(map(lambda it: '|' + ''.join(it) + '|', grid))
 
     def __repr__(self) -> str:
         return f'Tetris({self.grid})'
@@ -80,11 +88,35 @@ class Tetris:
                 if self.floating.shape[dy][dx]:
                     self.grid[y + dy][x + dx] = self.floating.shape[dy][dx]
         self.floating_pos = (0, 4)
-        self.floating = self.upcoming.pop()
+        self.floating = self.upcoming.pop().cloned()
         if not self.upcoming:
-            self.upcoming = TetrisPiece.all
+            self.upcoming = list(TetrisPiece.all)
             random.shuffle(self.upcoming)
         return self._consistent()
+
+    def rotate_clockwise(self) -> bool:
+        assert self._consistent()
+        self.floating.rotate_clockwise()
+        for kick in self.floating.kick_clockwise[-1]:
+            self.floating_pos = (self.floating_pos[0] + kick[0], self.floating_pos[1] + kick[1])
+            if self._consistent():
+                return True
+            else:
+                self.floating_pos = (self.floating_pos[0] - kick[0], self.floating_pos[1] - kick[1])
+        self.rotate_counterclockwise()
+        return False
+
+    def rotate_counterclockwise(self) -> bool:
+        assert self._consistent()
+        self.floating.rotate_counterclockwise()
+        for kick in self.floating.kick_counterclockwise[-1]:
+            self.floating_pos = (self.floating_pos[0] + kick[0], self.floating_pos[1] + kick[1])
+            if self._consistent():
+                return True
+            else:
+                self.floating_pos = (self.floating_pos[0] - kick[0], self.floating_pos[1] - kick[1])
+        self.rotate_clockwise()
+        return False
 
     # validity checks
     def _consistent(self) -> bool:
@@ -109,10 +141,29 @@ class Tetris:
 
 
 class TetrisPiece:
-    def __init__(self, shape: List[List[Color]]):
+    shape: List[List[Color]]
+    height: int
+    width: int
+    kick_clockwise: Deque[List[Tuple[int, int]]]
+    kick_counterclockwise: Deque[List[Tuple[int, int]]]
+
+    def __init__(
+            self,
+            shape: List[List[Color]],
+            kick_clockwise=None,
+            kick_counterclockwise=None
+    ):
+        if kick_clockwise is None:
+            kick_clockwise = deque([[(0, 0)]])
+        if kick_counterclockwise is None:
+            kick_counterclockwise = deque([[(0, 0)]])
+
         self.shape = shape
         self.height = len(shape)
         self.width = len(shape[0])
+        self.kick_clockwise = kick_clockwise
+        self.kick_counterclockwise = kick_counterclockwise
+
         for row in shape:
             assert len(row) == self.width
 
@@ -121,6 +172,17 @@ class TetrisPiece:
 
     def __repr__(self):
         return f'TetrisPiece({self.shape})'
+
+    def cloned(self) -> 'TetrisPiece':
+        return TetrisPiece(list(self.shape), deque(self.kick_clockwise), deque(self.kick_counterclockwise))
+
+    def rotate_clockwise(self):
+        self.kick_clockwise.rotate(-1)
+        self.kick_counterclockwise.rotate(1)
+
+    def rotate_counterclockwise(self):
+        self.kick_clockwise.rotate(1)
+        self.kick_counterclockwise.rotate(-1)
 
 
 i = Color('cyan')
@@ -132,32 +194,69 @@ t = Color('purple')
 z = Color('red')
 N = None
 
+# Arika SRS
+i_kicks = (
+    deque([
+        [(0,0),(-2,0),(+1,0),(+1,+2),(-2,-1)],  # 0->R
+        [(0,0),(-1,0),(+2,0),(-1,+2),(+2,-1)],  # R->2
+        [(0,0),(+2,0),(-1,0),(+2,+1),(-1,-1)],  # 2->L
+        [(0,0),(-2,0),(+1,0),(-2,+1),(+1,-2)],  # L->0
+    ]),
+    deque([
+        [(0,0),(+2,0),(-1,0),(-1,+2),(+2,-1)],  # 0->L
+        [(0,0),(+1,0),(-2,0),(+1,+2),(-2,-1)],  # L->2
+        [(0,0),(-2,0),(+1,0),(-2,+1),(+1,-2)],  # 2->R
+        [(0,0),(+2,0),(-1,0),(+2,+1),(-1,-2)],  # R->0
+    ]),
+)
+
+jlstz_kicks = (
+    deque([
+        [(0,0),(-1,0),(-1,+1),(0,-2),(-1,-2)],  # 0->R
+        [(0,0),(+1,0),(+1,-1),(0,+2),(+1,+2)],  # R->2
+        [(0,0),(+1,0),(+1,+1),(0,-2),(-1,-2)],  # 2->L
+        [(0,0),(-1,0),(-1,-1),(0,+2),(-1,+2)],  # L->0
+    ]),
+    deque([
+        [(0,0),(+1,0),(+1,+1),(0,-2),(+1,-2)],  # 0->L
+        [(0,0),(-1,0),(-1,-1),(0,+2),(-1,+2)],  # L->2
+        [(0,0),(-1,0),(-1,+1),(0,-2),(-1,-2)],  # 2->R
+        [(0,0),(+1,0),(+1,-1),(0,-2),(-1,-2)],  # R->0
+    ]),
+)
+
 TetrisPiece.I = TetrisPiece([[N, N, N, N],
                              [i, i, i, i],
                              [N, N, N, N],
-                             [N, N, N, N]])
+                             [N, N, N, N]],
+                            *i_kicks)
 TetrisPiece.J = TetrisPiece([[j, N, N],
                              [j, j, j],
-                             [N, N, N]])
+                             [N, N, N]],
+                            *jlstz_kicks)
 TetrisPiece.L = TetrisPiece([[N, N, l],
                              [l, l, l],
-                             [N, N, N]])
+                             [N, N, N]],
+                            *jlstz_kicks)
 TetrisPiece.O = TetrisPiece([[N, N, N, N],
                              [N, o, o, N],
                              [N, o, o, N],
                              [N, N, N, N]])
 TetrisPiece.S = TetrisPiece([[N, s, s],
                              [s, s, N],
-                             [N, N, N]])
+                             [N, N, N]],
+                            *jlstz_kicks)
 TetrisPiece.T = TetrisPiece([[N, t, N],
                              [t, t, t],
-                             [N, N, N]])
+                             [N, N, N]],
+                            *jlstz_kicks)
 TetrisPiece.Z = TetrisPiece([[z, z, N],
                              [N, z, z],
-                             [N, N, N]])
+                             [N, N, N]],
+                            *jlstz_kicks)
 TetrisPiece.Null = TetrisPiece([[]])
 
-del i, j, l, o, s, t, z, N
+del i, j, l, o, s, t, z, N, i_kicks, jlstz_kicks
 
 TetrisPiece.all = [TetrisPiece.I, TetrisPiece.J, TetrisPiece.L,
                    TetrisPiece.O, TetrisPiece.S,
